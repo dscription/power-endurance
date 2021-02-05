8f5d92f904e47d12a9e5b2db0b5027ab71344b43
dscription <dscriptionplayground@gmail.com>
2021-02-05 14:31:44 -0500

    Create ProblemTable component

--- src/pages/SessionSetup/SessionSetup.jsx
+++ src/pages/SessionSetup/SessionSetup.jsx
@@ -1,8 +1,14 @@
+import ProblemTable from '../../components/ProblemTable/ProblemTable';
+
 const SessionSetup = () => {
   return (
     <>
       <h1>Session Setup</h1>
-      <p>Suggested problems based off of your onsight and limit grades. Feel free to adjust the grades and wall type for this session.</p>
+      <p>
+        Suggested problems based off of your onsight and limit grades. Feel free
+        to adjust the grades and wall type for this session.
+      </p>
+      <ProblemTable />
     </>
   );
 };
