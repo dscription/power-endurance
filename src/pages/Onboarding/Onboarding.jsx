1ab8fd0cac7a2212d982ae1b72ef9c70d6d4bbc5
dscription <dscriptionplayground@gmail.com>
2021-02-05 18:18:17 -0500

    mockup onboarding page with inputs

--- src/pages/Onboarding/Onboarding.jsx
+++ src/pages/Onboarding/Onboarding.jsx
@@ -1,10 +1,44 @@
 import React, { useState, useEffect } from 'react';
 
 const Onboarding = ({ name }) => {
+  const [onsight, setOnsight] = useState(0);
+  const [limit, setLimit] = useState(0);
   return (
     <>
       <h1>Onboarding</h1>
       <h2>Welcome {name}</h2>
+      <p>What grade can you consistently onsight?</p>
+      <select
+        name="onsight"
+        value={onsight}
+        onChange={(event) => setOnsight(event.target.value)}
+      >
+        <option value={0}>V0</option>
+        <option value={1}>V1</option>
+        <option value={2}>V2</option>
+        <option value={3}>V3</option>
+        <option value={4}>V4</option>
+        <option value={5}>V5</option>
+        <option value={6}>V6</option>
+        <option value={7}>V7</option>
+        <option value={8}>V8</option>
+      </select>
+      <p>What grade would you consider to be your Limit? (projecting)</p>
+      <select
+        name="limit"
+        value={limit}
+        onChange={(event) => setLimit(event.target.value)}
+      >
+        <option value={0}>V0</option>
+        <option value={1}>V1</option>
+        <option value={2}>V2</option>
+        <option value={3}>V3</option>
+        <option value={4}>V4</option>
+        <option value={5}>V5</option>
+        <option value={6}>V6</option>
+        <option value={7}>V7</option>
+        <option value={8}>V8</option>
+      </select>
     </>
   );
 };
