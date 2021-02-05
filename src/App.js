b3c18971b64f9b3bf148e871f85f9fc3905b1fd0
dscription <dscriptionplayground@gmail.com>
2021-02-05 14:19:40 -0500

    set user SampleData to state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-
+import { user } from './SampleData';
+import React, {useState} from 'react';
+import SessionSetup from './pages/SessionSetup/SessionSetup';
 
 function App() {
+  const [userData, setUserData] = useState(user)
   return (
     <div>
-      Starter
+      {userData.name}
+      <SessionSetup />
     </div>
   );
 }
