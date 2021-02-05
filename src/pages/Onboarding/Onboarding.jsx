45efbb68aee75e6d3c927f7ecccf09d7f8cb4877
dscription <dscriptionplayground@gmail.com>
2021-02-05 16:44:02 -0500

    Onboarding page render user name from SampleData

--- src/pages/Onboarding/Onboarding.jsx
+++ src/pages/Onboarding/Onboarding.jsx
@@ -1,7 +1,12 @@
 import React, { useState, useEffect } from 'react';
 
-const Onboarding = () => {
-  return <h1>Onboarding</h1>;
+const Onboarding = ({ name }) => {
+  return (
+    <>
+      <h1>Onboarding</h1>
+      <h2>Welcome {name}</h2>
+    </>
+  );
 };
 
 export default Onboarding;
