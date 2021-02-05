f8b07530888476c7f46323820466749696a1ebd3
dscription <dscriptionplayground@gmail.com>
2021-02-05 14:27:35 -0500

    add flexbox and padding to Container

--- src/components/styled/Container.jsx
+++ src/components/styled/Container.jsx
@@ -1,5 +1,10 @@
 import styled from 'styled-components'
 
 export const Container = styled.main`
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  padding: 0px 10px;
+  /* this main container should also account for mobile first sizing. */
 
 `
