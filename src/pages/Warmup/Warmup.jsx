2d73ae2772602d79e1abbcd018ccd40b35c03643
dscription <dscriptionplayground@gmail.com>
2021-02-06 14:08:47 -0500

    add markup to Warmup component

---
+++ src/pages/Warmup/Warmup.jsx
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from 'react';
+
+const Warmup = ({ session }) => {
+  // const [round,setRound] = useState(0)
+
+  return (
+    <>
+      <h1>Warmup: Round 1 </h1>
+      <p>Climb 6 x v0 or v1 with minimal rest between climbs and click next.</p>
+      <button>Next</button>
+    </>
+  );
+};
+
+export default Warmup;
