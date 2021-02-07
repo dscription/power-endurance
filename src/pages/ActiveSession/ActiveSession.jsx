df472108fd046a3ce4f5ee7da0f28a057119da67
dscription <dscriptionplayground@gmail.com>
2021-02-07 14:16:11 -0500

    add markup and backwards timer to ActiveSessionWork component

--- src/pages/ActiveSession/ActiveSession.jsx
+++ src/pages/ActiveSession/ActiveSession.jsx
@@ -1,7 +1,11 @@
 import React, { useState, useEffect } from 'react';
+import ActiveSessionWork from '../../components/ActiveSessionWork/ActiveSessionWork';
 
-const ActiveSession = () => {
-  return ( <></> );
+const ActiveSession = ({session}) => {
+  const currentRound = session.rounds[0]
+  return ( <>
+    <ActiveSessionWork problems={currentRound.problems} />
+  </> );
 }
  
 export default ActiveSession;
