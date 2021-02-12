4e9f78085b976316d6dfe86349b666b429572a4f
Dan Boterashvili <danb.dul@gmail.com>
2021-02-11 19:20:34 -0500

    server is working and users can be created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
 import { user } from './SampleData';
+import { Route, Redirect } from 'react-router-dom';
 import React, { useState, useEffect } from 'react';
 import SessionSetup from './pages/SessionSetup/SessionSetup';
 import { Container } from './components/styled/Container';
 import Warmup from './pages/Warmup/Warmup';
 import ActiveSession from './pages/ActiveSession/ActiveSession';
+import Signup from './pages/Signup/Signup';
+import Login from './pages/Login/Login';
+import NavBar from './components/NavBar/NavBar'
+import authService from './services/authService';
 
 function App() {
-  const [userData, setUserData] = useState('');
+  const [user, setUser] = useState('');
 
-  useEffect(() => {
-    setUserData(user);
-  }, [user]);
+  // useEffect(() => {
+  //   setUserData(user);
+  // }, [user]);
+
+  const handleLogout = () => {
+    authService.logout();
+    setUser({ user: null });
+  };
+
+  const handleSignupOrLogin = () => {
+    setUser({ user: authService.getUser() });
+    // this.setState({ user: authService.getUser() });
+  };
 
   return (
-    <Container>
-      {/* {userData && <Warmup session={userData.session}/>} */}
-      {/* {userData && <SessionSetup session={userData.session} />} */}
-      {userData && <ActiveSession session={userData.session} />}
-    </Container>
+    <>
+      <NavBar user={user} handleLogout={handleLogout} />
+      <Route
+        exact
+        path="/"
+        render={() => (
+          <Container>
+            {/* {userData && <Warmup session={userData.session}/>} */}
+            {/* {userData && <SessionSetup session={userData.session} />} */}
+            {/* {userData && <ActiveSession session={userData.session} />} */}
+          </Container>
+        )}
+      />
+      <Route
+        exact
+        path="/signup"
+        render={({ history }) => (
+          <Signup history={history} handleSignupOrLogin={handleSignupOrLogin} />
+        )}
+      />
+      <Route
+        exact
+        path="/login"
+        render={({ history }) => (
+          <Login history={history} handleSignupOrLogin={handleSignupOrLogin} />
+        )}
+      />
+    </>
   );
 }
 
