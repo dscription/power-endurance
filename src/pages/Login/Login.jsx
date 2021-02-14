3609a3baabec9aea26e36e9d8e2814b32ad8934e
Dan Boterashvili <danb.dul@gmail.com>
2021-02-14 13:26:03 -0500

    initial styling of login page

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,12 +1,22 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
-import "./Login.css";
-import authService from "../../services/authService"
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import authService from '../../services/authService';
+
+import styled from 'styled-components';
+
+const Main = styled.main`
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: center;
+  height: '90vh';
+  /* justify-content: center; */
+  /* align-items: center;  */
+`;
 
 class LoginPage extends Component {
   state = {
-    email: "",
-    pw: "",
+    email: '',
+    pw: '',
   };
 
   handleChange = (e) => {
@@ -22,7 +32,7 @@ class LoginPage extends Component {
       await authService.login(this.state);
       // Let <App> know a user has signed up!
       handleSignupOrLogin();
-      history.push("/");
+      history.push('/');
     } catch (err) {
       // Use a modal or toast in your apps instead of alert
       alert('Invalid Credentials!');
@@ -30,35 +40,39 @@ class LoginPage extends Component {
   };
 
   render() {
-    const {email, pw} = this.state
+    const { email, pw } = this.state;
     return (
-      <main className="Login">
+      <Main>
         <h3>Log In</h3>
         <form autoComplete="off" onSubmit={this.handleSubmit}>
-          <input
-            type="text"
-            autoComplete="off"
-            id="email"
-            value={email}
-            name="email"
-            onChange={this.handleChange}
-          />
-          <label htmlFor="email">Email</label>
-          <input
-            type="password"
-            autoComplete="off"
-            id="password"
-            value={pw}
-            name="pw"
-            onChange={this.handleChange}
-          />
-          <label htmlFor="password">Password</label>
+          <div>
+            <input
+              type="text"
+              autoComplete="off"
+              id="email"
+              value={email}
+              name="email"
+              onChange={this.handleChange}
+            />
+            <label htmlFor="email">Email</label>
+          </div>
+          <div>
+            <input
+              type="password"
+              autoComplete="off"
+              id="password"
+              value={pw}
+              name="pw"
+              onChange={this.handleChange}
+            />
+            <label htmlFor="password">Password</label>
+          </div>
           <button className="btn green">Log In</button>&nbsp;&nbsp;&nbsp;
           <Link className="btn red" to="/">
             Cancel
           </Link>
         </form>
-      </main>
+      </Main>
     );
   }
 }
